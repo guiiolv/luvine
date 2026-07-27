cbcc6aab34964239b1f4d92415fc5bbd987a84ab
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-07-27 16:54:39 -0300

    first commit

---
+++ src/test/java/com/luvine/LuvineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.luvine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuvineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
