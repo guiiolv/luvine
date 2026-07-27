a595b02f9d5fc302bfddf6e81d93a3b0e604a2d6
Guilherme Oliveira <guiisilvaax@gmail.com>
2026-07-27 20:54:42 -0300

    add: V2__create_auth_schema.sql

---
+++ src/main/resources/db/migration/V2__create_auth_schema.sql
@@ -0,0 +1,18 @@
+CREATE SCHEMA IF NOT EXISTS aut;
+
+CREATE SEQUENCE auth.refresh_tokens_id_seq START WITH 1 INCREMENT BY 50;
+
+CREATE TABLE auth.refresh_tokens(
+    id BIGINT PRIMARY KEY DEFAULT nextval('auth.refresh_tokens_id_seq'),
+    user_public_id UUID NOT NULL,
+    token_hash VARCHAR(255) UNIQUE NOT NULL,
+    device_info VARCHAR(255) NOT NULL,
+    ip_address VARCHAR(100) NOT NULL,
+    expires_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    revoked BOOLEAN NOT NULL,
+    replaced_by_token_hash VARCHAR(255),
+    created_at TIMESTAMP WITH TIME ZONE
+);
+
+CREATE INDEX idx_refresh_tokens_user_public_id ON auth.refresh_tokens(user_public_id);
+CREATE INDEX idx_refresh_tokens_replaced_by_token_hash ON auth.refresh_tokens(replaced_by_token_hash);
